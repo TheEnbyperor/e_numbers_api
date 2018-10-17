d29da6415066757bcee95820c73a2dcfdd95921a
Benjamin Misell <q@misell.cymru>
2018-10-17 10:27:44 +0100

    Initial commit

---
+++ api/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    more_info = models.URLField()
+
+    class Meta:
+        verbose_name_plural = "Categories"
+
+    def __str__(self):
+        return self.name
+
+
+class SubCategory(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='sub_categories')
+    name = models.CharField(max_length=255)
+    more_info = models.URLField()
+
+    class Meta:
+        verbose_name_plural = "Sub categories"
+
+    def __str__(self):
+        return self.name
+
+
+class Substance(models.Model):
+    sub_category = models.ForeignKey(SubCategory, on_delete=models.CASCADE, related_name='substances')
+    name = models.CharField(max_length=255)
+    more_info = models.URLField()
+
+    def __str__(self):
+        return self.name
